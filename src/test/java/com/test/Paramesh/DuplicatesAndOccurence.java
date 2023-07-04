ba50c890a830f286604ee22188a421002ba13a11
saitejain <saitejain9@gmail.com>
2023-07-04 17:07:36 +0530

    New Programs tester 4

---
+++ src/test/java/com/test/Paramesh/DuplicatesAndOccurence.java
@@ -0,0 +1,22 @@
+package com.test.Paramesh;
+import java.util.*;
+
+public class DuplicatesAndOccurence {
+	
+	public static void duplicateWords(String str) {
+		String [] sp = str.split(" ");
+		HashMap<String,Integer> charCount = new HashMap<>();
+		for(String words:sp) {
+			if(charCount.containsKey(words.toLowerCase())) {
+			
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
