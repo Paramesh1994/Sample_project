159ae882c15d03642bb68c37f859a8954c858405
Parameswara Reddy <Parameswara Reddy@LAPTOP-188TAOSL>
2023-07-04 19:23:10 +0530

    My_Branch commit

---
+++ src/main/java/com/test/Paramesh/App3.java
@@ -0,0 +1,23 @@
+package com.test.Paramesh;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App3 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+        
+    	WebDriver driver=new ChromeDriver();
+    	Thread.sleep(5000);
+    	driver.quit();
+    	
+    	System.out.println( "Hello Paramesh reddy Welcome to New World!" );
+    	System.out.println( "Hello World!" );
+    	System.out.println( "Hi Paramesh" );
+    }
+}
