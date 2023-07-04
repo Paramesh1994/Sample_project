a144886731d5efee7effb471cbaad8d1186c3246
Parameswara Reddy <Parameswara Reddy@LAPTOP-188TAOSL>
2023-07-04 15:30:43 +0530

    First commit

---
+++ src/main/java/com/test/Paramesh/App.java
@@ -0,0 +1,21 @@
+package com.test.Paramesh;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+        
+    	WebDriver driver=new ChromeDriver();
+    	Thread.sleep(5000);
+    	driver.quit();
+    	
+    	System.out.println( "Hello Paramesh reddy Welcome to New World!" );
+    }
+}
